812d448c0a011100d4faccc1785c49a07aa8ce79
Ganesh <59595508+Ganeshh1@users.noreply.github.com>
2020-11-24 15:41:22 +0530

    Update views.py

--- TicketBooking/MovieTicketBooking/views.py
+++ TicketBooking/MovieTicketBooking/views.py
@@ -21,7 +21,7 @@ def home(request):
         'movies':Movies.objects.all(),
         'Theater':Theater.objects.all(),
         'latest_movies':Movies.objects.all()[:],
-        # Verify Here 
+        
        
         
     }
@@ -31,10 +31,8 @@ def home(request):
 def Theater_Display(request):
     global current_TheaterName
     if request.method=='POST':
-        print('helloooooooooooo')
         name=request.POST.get('Theater_name')
         current_TheaterName=str(name)
-        print('1st',current_TheaterName)
         return redirect('movies/')
 
     context={
@@ -48,44 +46,24 @@ def Theater_Display(request):
 @login_required
 def Disp(request):
     global current_show,current_TheaterName,curret_movie_name
-    print('here',current_show,current_TheaterName)
-    y=Theater.objects.get(name=current_TheaterName)
-    x=Movies.objects.filter(theater_name=y) 
+    theater=Theater.objects.get(name=current_TheaterName)
+    movie=Movies.objects.filter(theater_name=theater) 
     print(current_TheaterName)
-    print('He',x)
-    print('Chudura',y)
-    curret_movie_name=y
+    curret_movie_name=movie
     
 
     if request.method =='POST':
         
         print(request.POST)
-        name=request.POST.get('name')
+        moviename=request.POST.get('name')
         print(current_show)
-        print(name)
-        movie_name=Movies.objects.get(name=name)
-        print(movie_name)
+        movie_name=Movies.objects.get(name=moviename)
         current_show=ShowTiming.objects.filter(movie=movie_name,showtime__gte=datetime.now())
-        print(current_show)
-
-        print(movie_name,'MOVIENAME**************')
-        # current_show = movie_name
-        # context={
-        #     'movies':Movies.objects.filter(theater_name=Theater.objects.get(name=str(name[0]))),
-        #     'timing':ShowTiming.objects.all(),
-        #         }
-        #     return render(request,'MovieTicketBooking/Movies.html',context)
-
         context={
         'movie':movie_name,
         'timing':current_show,
-        
-         }
+        }
 
-        # current_show_time=ShowTiming.objects.filter(showtime=current_show)
-        # print(context['movies'])
-        # print(context['timing'])
-        # print(current_show_time)
         return render(request,'MovieTicketBooking/show.html',context)
 
     context={
@@ -99,33 +77,26 @@ def Disp(request):
 @login_required
 def Show(request):
     global current_show,curret_movie_name,Final_movie
-    print(current_show,'000000000000000',curret_movie_name)
     
     if request.method=='POST':
         print(request.POST)
         ticket_timing=request.POST['number']
-        a=TotalCount.objects.get(Tickets=ShowTiming.objects.get(id=ticket_timing ))#doubte
-        print(a)
+        show=TotalCount.objects.get(Tickets=ShowTiming.objects.get(id=ticket_timing ))
+        print(show)
         context={
         
-        'show':a,
+        'show':show,
          }
-        Final_movie=a 
+        Final_movie=show 
         if(Final_movie.count<=0):
             return render(request,'MovieTicketBooking/error.html',{})
 
-        print(context)
-        # return HttpResponse('Hello')
         return render(request,'MovieTicketBooking/conform.html',context)
 
     context={
         'movies':Movies.objects.get(name=curret_movie_name),
         'show':TotalCount.objects.filter(),
     }
-    # print(context['movies'])
-    print(current_show,'0000000')
-    print(context['Theater'])
-    # print(context['show'])
     return render(request,'MovieTicketBooking/show.html',context)
 
 @login_required
@@ -135,10 +106,10 @@ def Confirm(request):
     if request.method=='POST':
         count=int(request.POST['count'])
         print(count,Final_movie.count)
-        a=Final_movie.count
+        count_final=Final_movie.count
         Final_movie.count-=count
         if(Final_movie.count<0):
-            Final_movie.count=a
+            Final_movie.count=count_final
             return render(request,'MovieTicketBooking/error.html',{})
         messages.warning(request,"Tickets Booked Succeesfully!")
         Final_movie.save()
@@ -173,7 +144,6 @@ def loginPage(request):
     if request.method == 'POST':
         username=request.POST.get('username')
         password=request.POST.get('password')
-        print('sajndlajksnlasndl',username,password)
 
         user= authenticate(request,username=username,password=password)
         if( user is not None):
