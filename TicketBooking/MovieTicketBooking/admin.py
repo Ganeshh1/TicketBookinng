402581fedad6b1b88e799af4e58108cc3f83e132
Ganesh <59595508+Ganeshh1@users.noreply.github.com>
2020-12-01 17:45:49 +0530

    Update admin.py

--- TicketBooking/MovieTicketBooking/admin.py
+++ TicketBooking/MovieTicketBooking/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Movies,Theater,TotalCount,ShowTiming
+from .models import Movies,Theater,ShowTiming
 
-admin.register(Movies,Theater,TotalCount,ShowTiming)(admin.ModelAdmin)
+admin.register(Movies,Theater,ShowTiming)(admin.ModelAdmin)
 # Register your models here.
