4fde0454a7859d57e59fc992fa6a3d7f879bb056
Ganesh <59595508+Ganeshh1@users.noreply.github.com>
2020-12-01 17:46:54 +0530

    Update models.py

--- TicketBooking/MovieTicketBooking/models.py
+++ TicketBooking/MovieTicketBooking/models.py
@@ -1,23 +1,14 @@
 from django.db import models
 import datetime
 
-
-
-
-
-
 class Theater(models.Model):
     name=models.CharField(max_length=100)
     
     def __str__(self):
         return self.name
 
-
-
 class Movies(models.Model):
-    name=models.CharField(max_length=100)
-
-    
+    name=models.CharField(max_length=100)    
     movie_image=models.ImageField(default='default.jpg',upload_to='movies')
     theater_name=models.ForeignKey(Theater,on_delete=models.CASCADE)
 
@@ -28,21 +19,15 @@ class Movies(models.Model):
         verbose_name_plural = "Movies"
         ordering = ['name']
 
-
 class ShowTiming(models.Model):
     showtime=models.DateTimeField(default=datetime.datetime.now())
     movie=models.ForeignKey(Movies,on_delete=models.CASCADE)
-    
-
-    def __str__(self):
-        return str(str(self.showtime)+ ' '+ str(self.movie))
-
-
-
-class TotalCount(models.Model):
     count=models.IntegerField(default=150)
-    Tickets=models.ForeignKey(ShowTiming,on_delete=models.CASCADE)
+
     def __str__(self):
-        return str(self.count)+' '+str(self.Tickets)
+        return str(str(self.showtime)+ ' '+ str(self.movie)+' '+str(self.count))
+
+
+
 
 
